0f58395aa1f111138127e351d8b3e191c9d3c32f
Dongliang Peng <pengdlzn@gmail.com>
2020-11-18 12:27:05 +0100

    added 11/interface/, where the interface of setting is improved

---
+++ 2020/11/interface/htmlfuns.js
@@ -0,0 +1,87 @@
+function enhanceMenu() {
+    /* opening and closing the settings modal dialog */
+    // FIXME: repetition of names and ids here is cumbersome
+    var toggleSettingsButton = function () {
+        //toggleMenu(); // hide the menu
+        toggleSettings()
+        //document.getElementById('settingsModal').style.visibility = 'visible';
+    };
+    document.getElementById("toggleSettingsButton").addEventListener('click', toggleSettingsButton);
+
+    //The below code is for the cross of the setting panel, where the cross is used to close the panel
+    //var modalSettingsClose = function () {
+        
+    //    document.getElementById('settingsModal').style.visibility = 'hidden';
+    //}
+    //document.getElementById("closeSettingsButton").addEventListener('click', modalSettingsClose);
+    //document.getElementById("closeSettingsSpan").addEventListener('click', modalSettingsClose);
+
+    //trigger the click event of openSettingsButton
+    //const event_modalSettings = new Event('click')
+    //document.getElementById("openSettingsButton").dispatchEvent(event_modalSettings)
+    document.getElementById('settingsModal').style.visibility = 'visible';
+    // the menu on the top
+    function toggleMenu() {
+        var x = document.getElementById("menu");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    };
+
+    function toggleSettings() {        
+        var x = document.getElementById("settingsModal");
+        if (x.style.visibility == 'visible') {
+            x.style.visibility = 'hidden'
+        }
+        else {
+            x.style.visibility = 'visible'
+        }
+
+
+        //if (x.className.indexOf("w3-show") == -1) {
+        //    x.className += " w3-show";
+        //} else {
+        //    x.className = x.className.replace(" w3-show", "");
+        //}
+    };
+
+    //const event_toggleMenu = new Event('click')
+    //document.getElementById("toggleMenu").addEventListener('click', toggleMenu);
+    //document.getElementById("toggleMenu").dispatchEvent(event_toggleMenu)
+
+    /* -- start slider -- */
+    let init_slider = (name) => {
+        let msgbus = new varioscale.MessageBusConnector();
+        let event_nm = "settings." + name  //e.g., event_nm: "settings.zoom-factor"
+        msgbus.subscribe(event_nm, (topic, message, sender) => {
+            let el = document.getElementById(name + "-value");
+            el.innerHTML = message;
+        });
+        let slider = document.getElementById(name);
+        slider.addEventListener('input', () => {
+            msgbus.publish(event_nm, parseFloat(slider.value));
+        });
+        msgbus.publish(event_nm, parseFloat(slider.value));
+    };
+    init_slider("zoom-factor");
+    init_slider("zoom-duration");
+    init_slider("pan-duration");
+    init_slider("boundary-width");
+    /* -- end of slider -- */
+
+
+
+    //test to put this function into layercontrol.js
+    function toggleLegend() {
+        var x = document.getElementById("Demo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+}
+
+
